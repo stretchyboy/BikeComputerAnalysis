a798a4e0ec980d62ae98cf8e4988f4653696fcc5
Martyn Eggleton <martyn.eggleton@gmail.com>
2024-07-22 11:34:15 +0100

    Data clened and organised

--- session_analysis.py
+++ session_analysis.py
@@ -19,7 +19,8 @@ from geopy.point import Point
 
 con = sqlite3.connect("data/backuped_sessions.db")
 #df = pd.read_sql_query("SELECT session_id, lat/1000000.0 as lat, lon/1000000.0 as lon, elev, time, temperature, distance, starttime, overalltime, meters as elevation_gain, weight as bike_weight FROM tracks JOIN sessions ON (tracks.session_id = sessions._id) JOIN bikes ON (used_bike=bikes._id) WHERE cat IN (1,2,3) AND sessions._id = 34", con)
-df = pd.read_sql_query("SELECT session_id, lat/1000000.0 as lat, lon/1000000.0 as lon, elev, time, temperature, distance, starttime, overalltime, meters as elevation_gain, weight as bike_weight FROM tracks JOIN sessions ON (tracks.session_id = sessions._id) JOIN bikes ON (used_bike=bikes._id) WHERE cat IN (1,2,3)", con)
+#exclude comutting ang heavy bike days
+df = pd.read_sql_query("SELECT session_id, lat/1000000.0 as lat, lon/1000000.0 as lon, elev, time, temperature, distance, starttime, overalltime, meters as elevation_gain, weight as bike_weight FROM tracks JOIN sessions ON (tracks.session_id = sessions._id) JOIN bikes ON (used_bike=bikes._id) WHERE cat IN (1,2,3) AND weight <16", con)
 
 
 def haversine(lat1, lon1, lat2, lon2):
@@ -40,15 +41,22 @@ def haversine(lat1, lon1, lat2, lon2):
 
 df['len'] = haversine(df['lat'].shift(), df['lon'].shift(), df['lat'], df['lon'])
 df['dist'] = df['len'].cumsum()
-#df['elapsed_mins'] = ((df['time'] - df['starttime']) / (1000*60) ).astype(int)
-df['elapsed_hrs'] = ((df['time'] - df['starttime']) / (1000*60*60) ).astype(int)
-
-
-# Verify that result of SQL query is stored in the dataframe
+df['pre_mins'] = ((df['time'] - df['starttime']) / (1000*60) ).astype(int)
+df['pre_hours'] = ((df['time'] - df['starttime']) / (1000*60*60) ).astype(int)
 
 df['grad'] = 100 * (df['elev']- df['elev'].shift()) / df['len']
 df['id'] = (df['elev'].diff(10) < 0).cumsum()
 #df['id'] = (df['grad'] < 0).cumsum()
+
+df['gained'] = (df['elev'].diff()>0).cumsum()
+
+sessions = df.groupby(['session_id']).agg(
+        dist_start=('dist', "min"),
+        pre_gained=('gained', "min"),
+        )
+
+
+# Verify that result of SQL query is stored in the dataframe
 
 
 df.fillna(0, inplace=True)
@@ -60,6 +68,8 @@ groups = df.groupby(['session_id','id']).agg(
         dist_end=('dist', "max"),
         #count = ("id","count"),
         #gradient_max=('grad', "max"),
+        pre_gained=('gained', "min"),
+        
         ele_start=('elev', "min"),
         ele_end=('elev', "max"),
         time_start=('time', "min"),
@@ -67,14 +77,22 @@ groups = df.groupby(['session_id','id']).agg(
         #mid_lat = ("lat","mean"),
         #mid_lon = ("lon","mean"),
         #elapsed_mins = ('elapsed_mins', "min"),
-        elapsed_hrs = ('elapsed_hrs', "min"),
+        pre_mins = ('pre_mins', "min"),
+        pre_hours = ('pre_hours', "min"),
+        temp = ('temperature', 'mean')
     )
 
 groups.fillna(0, inplace=True)
 
 
-groups['dist_start'] = groups['dist_start'].astype(int)
-groups['dist_end'] = groups['dist_end'].astype(int)
+groups['dist_start'] = (groups['dist_start']-sessions['dist_start']).astype(int)
+groups['dist_end'] = (groups['dist_end']-sessions['dist_start']).astype(int)
+
+groups['pre_gained'] = (groups['pre_gained']-sessions['pre_gained']).astype(int)
+
+#groups['dist_start'] = groups['dist_start'].astype(int)
+#groups['dist_end'] = groups['dist_end'].astype(int)
+
 
 groups = groups[groups['dist_end']>groups['dist_start']]
 groups = groups[groups['ele_end']>groups['ele_start']]
@@ -82,7 +100,7 @@ groups = groups[groups['ele_end']>groups['ele_start']]
 
 groups['grad_mean'] = (100 * (groups["ele_end"]- groups["ele_start"]) / (groups['dist_end'] - groups['dist_start'])).astype(int)
 
-groups = groups[(groups['grad_mean'] < 20)] 
+groups = groups[(groups['grad_mean'] < 30)] 
 
 groups['dur'] = ((groups["time_end"]- groups["time_start"]) / 1000).astype(int)
 groups['length'] = groups['dist_end'] - groups['dist_start']
@@ -96,17 +114,30 @@ groups = groups[groups['kph'] > 5]
 groups.drop(labels=['id','dist_start', 'dist_end', 'ele_start', 'ele_end', 'time_start','time_end'], axis=1, errors='ignore', inplace=True)
 
 groups['score'] = ((groups['length']) * groups['grad_mean']).astype(int)
+groups['pre_score'] = groups['score'].cumsum() - groups['score']
 
+sess2 = groups.groupby(['session_id']).agg(
+        min_pre_score=('pre_score', "min"),
+)
+print(sess2)
+
+groups['pre_score'] = groups['pre_score'] - sess2['min_pre_score']
 #The filter used for the bike computer
 #groups = groups[(groups['score'] > 6500)] 
 
-groups["cat"] = pd.cut(groups['score'], [0, 8000, 16000, 32000, 64000, 80000, 1000000], right=True, labels=["-","4", "3", "2", "1", "HC"])
+groups["cat"] = pd.cut(groups['score'], [-100000, 8000, 16000, 32000, 64000, 80000, 1000000], right=True, labels=["-","4", "3", "2", "1", "HC"])
 
 print (groups)
 
-con.close()
+data = groups.reset_index(drop=True)#
 
-means = groups.groupby(['elapsed_hrs','grad_mean']).agg(
+
+#.drop(labels=["session_id", "id"])
+print (data)
+con.close()
+exit()
+
+means = groups.groupby(['pre_hours','grad_mean']).agg(
         kph = ("kph","mean"),
         #kph_min = ("kph","min"),
         #kph_max = ("kph","max"),
@@ -114,3 +145,18 @@ means = groups.groupby(['elapsed_hrs','grad_mean']).agg(
 print(means.unstack())
 
 # Look at normalising againt each element and then multipling up
+
+grads = groups.groupby(['grad_mean']).agg(
+        kph = ("kph","mean"),
+        #kph_min = ("kph","min"),
+        #kph_max = ("kph","max"),
+)
+print(grads)
+
+
+hours = groups.groupby(['pre_hours']).agg(
+        kph = ("kph","mean"),
+        #kph_min = ("kph","min"),
+        #kph_max = ("kph","max"),
+)
+print(hours)
